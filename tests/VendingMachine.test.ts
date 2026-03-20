5f8b588c8d9fda2d3464b17192802478c395310b
ErwinPz <pzocikerwin@gmail.com>
2026-03-20 01:00:07 -0300

    primer test de prueba

--- tests/VendingMachine.test.ts
+++ tests/VendingMachine.test.ts
@@ -1,1 +1,8 @@
 import { VendingMachine } from '../src/VendingMachine';
+
+describe('VendingMachine', () => {
+    test('al inicializar la maquina esta empieza con 0 de credito', () => {
+        const machine = new VendingMachine();
+        expect(machine.getCurrentCredit()).toBe(0);
+    });
+});
