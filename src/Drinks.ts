5ab673e17ccf678ee27ffbcb515512c092183496
ErwinPz <pzocikerwin@gmail.com>
2026-03-21 23:49:05 -0300

    aplicamos herencia

---
+++ src/Drinks.ts
@@ -0,0 +1,12 @@
+import { Product } from "./Product";
+
+export class Drinks extends Product{
+
+public volume: number;
+
+constructor(name: string, price: number, quantity: number, weight: number, volume: number){
+    super(name, price, quantity, weight); //Llamada al constructor de la clase padre (Product). El constructor de la clase hija recibe todos los valores y dentro del constructor de la hija con super la clase padre se encarga de la asignacion de estas.
+    this.volume = volume;
+}
+
+}
